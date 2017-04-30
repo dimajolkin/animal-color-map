31cee27d60615e85a4f5deba4e1a4cd5857b3e6d
aser <aser@ASER->
2017-04-30 23:20:45 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.CausesValidation = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(436, 27);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(404, 414);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dataGridView1_RowsRemoved);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.runButton,
+            this.обновитьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(840, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // runButton
+            // 
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(81, 20);
+            this.runButton.Text = "Выполнить";
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // обновитьToolStripMenuItem
+            // 
+            this.обновитьToolStripMenuItem.Name = "обновитьToolStripMenuItem";
+            this.обновитьToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.обновитьToolStripMenuItem.Text = "Обновить";
+            this.обновитьToolStripMenuItem.Click += new System.EventHandler(this.обновитьToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(430, 345);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 447);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(840, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 473);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(840, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 431);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0%";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 378);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "5";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Пиксель";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Точность";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 378);
+            this.textBox2.MaxLength = 100;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "80000";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 495);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Beta MapColors";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripMenuItem runButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+
+    public partial class Form1 : Form
+    {
+        class ColorCount
+        {
+            private Color color;
+            private int count = 0;
+
+            public Color Color
+            {
+                get { return color; }
+                set { color = value; }
+            }
+
+            public int Count
+            {
+                get { return count; }
+                set { count = value; }
+            }
+
+
+            public void inc()
+            {
+                count++;
+            }
+
+            public int getRgb()
+            {
+                return color.R * color.G * color.B;
+            }
+
+            public ColorCount(Color color, int count = 0)
+            {
+                this.color = color;
+                this.count = count;
+            }
+        }
+
+        class ColorCountCollectin : List<ColorCount>
+        {
+            public int range = 80000;
+            List<Color> defaultColors;
+
+            public bool Contains(Color item)
+            {
+                foreach (ColorCount c in this)
+                {
+                    if (c.Color == item)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            private void inc(ColorCount c)
+            {
+                c.inc();
+            }
+
+            private List<Color> GetColors()
+            {
+                if (defaultColors == null)
+                {
+                    defaultColors = new List<Color>();
+                    string[] colorNames = Enum.GetNames(typeof(KnownColor));
+                    foreach (string colorName in colorNames)
+                    {
+                        KnownColor knownColor = (KnownColor)Enum.Parse(typeof(KnownColor), colorName);
+                        if (knownColor > KnownColor.Transparent)
+                        {
+                            defaultColors.Add(Color.FromName(colorName));
+                        }
+                    }
+                }
+
+                return defaultColors;
+            }
+
+            public void Add(Color item)
+            {
+                int rgb = item.R * item.G * item.B;
+                bool f = true;
+                this.ForEach(c => {
+                    if ((c.Color.ToArgb() - range < item.ToArgb()) && (c.Color.ToArgb() + range > item.ToArgb()))
+                    {
+                        inc(c);
+                        f = false;
+                    }
+                });
+
+                if (f)
+                {
+                    this.Add(new ColorCount(item));
+                }
+
+            }
+
+            public ColorCount GetByColor(Color color)
+            {
+                return this.Find(c => c.Color == color);
+            }
+
+            public void Remove(Color item)
+            {
+                this.RemoveAll(i => i.Color == item);
+            }
+        }
+
+        class Brush
+        {
+            public int stepX { get; set; }
+            public int stepY { get; set; }
+            public Brush(int x, int y)
+            {
+                this.stepX = x;
+                this.stepY = y;
+            }
+
+
+            public Color getMaxColor(Bitmap bmp, int x, int y)
+            {
+                ColorCountCollectin list = new ColorCountCollectin();
+                
+                for(int k = x; k < x + this.stepX; k++)
+                {
+                    for(int m = y; m < y + this.stepY; m++)
+                    {
+                        list.Add(bmp.GetPixel(k, m));
+                    }
+                }
+
+                list.Sort(delegate (ColorCount colorX, ColorCount colorY)
+                {
+                    if (colorX != colorY) return 0;
+                    if (colorX.Count == colorY.Count) return 0;
+                    if (colorX.Count > colorY.Count) return 1;
+                    if (colorX.Count < colorY.Count) return -1;
+                    return 0;
+                });
+
+                return list[0].Color;
+            }
+        }
+
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add("N", "N");
+            dataGridView1.Columns.Add("Color", "txt");
+            dataGridView1.Columns.Add("%", "%");
+        
+            runButton.Enabled = false;
+            statusStrip1.Text = "Готов к работе. Выбери файл";
+        }
+
+        private Bitmap bitmap;
+        Brush brush = new Brush(5, 5);
+        private int countPixels = 0;
+
+        private ColorCountCollectin colorCounts = new ColorCountCollectin();
+ 
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap bitmap = (Bitmap)e.Argument;
+            int lastProcent = 0;
+            for (int x = 0; x < bitmap.Width - brush.stepX; x+= brush.stepX)
+            {
+                for (int y = 0; y < bitmap.Height - brush.stepY; y+= brush.stepY)
+                {
+
+                    colorCounts.Add(brush.getMaxColor(bitmap, x, y));
+                }
+
+                int p = Convert.ToInt32((x * 1.0 / bitmap.Width) * 100);
+                if (lastProcent != p)
+                {
+                    backgroundWorker1.ReportProgress(p);
+                    lastProcent = p;
+                }
+                
+            }
+        }
+       
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    colorCounts.Clear();
+                    dataGridView1.Rows.Clear();
+                    bitmap = new Bitmap(dlg.FileName);
+                    countPixels = Convert.ToInt32(bitmap.Width / brush.stepX) * Convert.ToInt32(bitmap.Height / brush.stepY);
+
+                    pictureBox1.Image = (Image)bitmap;
+                         progressBar1.Value = progressBar1.Minimum;
+                         if (backgroundWorker1.IsBusy)
+                         {
+                              backgroundWorker1.CancelAsync();
+                         }
+                    statusStrip1.Text = "Вы выбрали файл: " + dlg.FileName;
+                    runButton.Enabled = true;
+                    
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = progressBar1.Value.ToString() + "%  цветов(" + colorCounts.Count  + ")";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = progressBar1.Maximum;
+            if (!e.Cancelled)
+            {
+                updateDataGrid(colorCounts, countPixels);
+            }
+
+            progressBar1.Value = progressBar1.Minimum;
+            label1.Text = "0%";
+        }
+
+        private void updateDataGrid(List<ColorCount> list, int countPixels)
+        {
+            dataGridView1.Rows.Clear();
+            foreach (ColorCount color in list)
+            {
+                double procent = (color.Count * 1.0 / (countPixels)) * 100;
+
+                var two = new DataGridViewRow();
+
+                var numberCell = new DataGridViewTextBoxCell();
+                numberCell.Value = dataGridView1.Rows.Count.ToString();
+
+                var colorCell = new DataGridViewTextBoxCell();
+                colorCell.Value = color.Color;
+                colorCell.Style.BackColor = color.Color;
+
+                var procentCell = new DataGridViewTextBoxCell();
+                procentCell.Value = Math.Round(procent, 3).ToString() + "%";
+
+                two.Cells.Add(numberCell);
+                two.Cells.Add(colorCell);
+                two.Cells.Add(procentCell);
+
+                dataGridView1.Rows.Add(two);
+            }
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            colorCounts.Clear();
+            dataGridView1.Rows.Clear();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            label1.Text = "0%";
+
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.RunWorkerAsync(bitmap);
+            statusStrip1.Text = "В работе.. ";
+            runButton.Enabled = false;
+        }
+
+        private void dataGridView1_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            
+        }
+
+        private void обновитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        public void update()
+        {
+            colorCounts.range = Convert.ToInt32(textBox2.Text);
+            brush = new Brush(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox1.Text));
+            countPixels = Convert.ToInt32(bitmap.Width / brush.stepX) * Convert.ToInt32(bitmap.Height / brush.stepY);
+
+            List<Color> dataGridViewColors = new List<Color>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                dataGridViewColors.Add(row.Cells[1].Style.BackColor);
+            }
+
+            List<ColorCount> removes = colorCounts.FindAll(c => !dataGridViewColors.Contains(c.Color));
+            colorCounts.RemoveAll(i => removes.Contains(i));
+            removes.ForEach(i => { countPixels -= i.Count; });
+
+            updateDataGrid(colorCounts, countPixels);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
